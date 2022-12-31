ec01252f20a88de88f7fddcec9d69df4b292c82c
cedricquitor <cedricquitor.dev@gmail.com>
2023-01-01 05:13:53 +0800

    Added navbar to about and contact

--- app/contact/layout.tsx
+++ app/contact/layout.tsx
@@ -1,3 +1,10 @@
+import Navbar from "../../components/Navbar";
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
 }
