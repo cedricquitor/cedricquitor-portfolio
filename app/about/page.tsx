d32f6d9c892632de0e6685ad8c76461670cf6105
cedricquitor <cedricquitor.dev@gmail.com>
2023-02-07 04:33:06 +0800

    Finished 20% of about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,20 +10,23 @@ const AboutMe = () => {
         <div>
           <div className="h-48 w-48 bg-[#37b47e] rounded-full" />
         </div>
-        <p>
-          Welcome to my portfolio project website! I&apos;m Cedric Quitor, a student at the University of Santo Tomas pursuing a degree in IT. My passion for technology and data drives me to continuously learn and expand my knowledge in the field. I
-          am particularly interested in data science and have a goal of pursuing a career in this exciting field after completing my bachelor&apos;s degree.
-        </p>
+        <div className="flex flex-col gap-y-2">
+          <p>
+            Welcome to my portfolio project website! I&apos;m <span className="font-bold">Cedric Quitor</span>, a student at the University of Santo Tomas pursuing a degree in Information Technology. My passion for technology and data drives me to
+            continuously learn and expand my knowledge in the field.
+          </p>
+          <p>I am particularly interested in Software Engineering and have a goal of pursuing a career in this exciting field after completing my bachelor&apos;s degree.</p>
+        </div>
       </div>
-      <div className="mt-4">
+      <div className="mt-4 flex flex-col gap-y-2">
         <p className="text-gray-500">
           Aside from academics, I am also keen on developing web, data, and AI/ML applications. I believe that technology has the power to solve some of the world&apos;s biggest challenges, and I am eager to contribute my own solutions.
         </p>
-        <p className="mt-2">
+        <p className="text-gray-500">
           As a student, I am always looking for new opportunities to learn and grow, and I am excited to see what the future holds for me in this constantly evolving field. I hope to make a difference by utilizing my technical skills and passion for
           technology to make a positive impact in the world.
         </p>
-        <p className="mt-2">Thank you for taking the time to visit my portfolio website. If you have any questions or would like to learn more about my work, don&apos;t hesitate to get in touch!</p>
+        <p className="text-gray-500">Thank you for taking the time to visit my portfolio website. If you have any questions or would like to learn more about my work, don&apos;t hesitate to get in touch!</p>
       </div>
       <Button className="transition duration-500 hover:-translate-y-2 hover:shadow-xl mt-6">Connect with me</Button>
     </div>
@@ -34,21 +37,31 @@ const Education = () => {
   return (
     <div className="mt-8">
       <h1 className="font-bold text-3xl">Education</h1>
-      <div>
-        <div className="flex">
-          <Image src={ust_logo} height={48} alt="University of Santo Tomas Logo" />
-          <Image src={ustcics_logo} height={48} alt="University of Santo Tomas College of Information and Computing Sciences Logo" />
+      <div className="mt-4 flex flex-col gap-y-4">
+        {/* TOOD: Make this card a component */}
+        <div className="outline outline-2 outline-gray-200 p-4 rounded-2xl flex justify-between items-center">
+          <div>
+            <h1 className="font-bold text-2xl text-[#37b47e]">University of Santo Tomas</h1>
+            <h2 className="font-bold text-sm text-[#37b47e]">College of Information and Computing Sciences</h2>
+            <h2 className="font-bold text-sm">Bachelor of Science in Information Technology</h2>
+            <h3 className="italic text-sm">Specialization in Web and Mobile Development</h3>
+          </div>
+          <div className="flex gap-x-4 justify-between w-[8rem]">
+            <Image src={ust_logo} height={56} alt="University of Santo Tomas Logo" />
+            <Image src={ustcics_logo} height={56} alt="University of Santo Tomas College of Information and Computing Sciences Logo" />
+          </div>
         </div>
-        <h1>University of Santo Tomas</h1>
-        <h2>Bachelor of Science in Information Technology</h2>
-      </div>
-      <div>
-        <div>
-          <h1>UST Logo</h1>
-          <h1>UST SHS Logo</h1>
+        <div className="outline outline-2 outline-gray-200 p-4 rounded-2xl flex justify-between items-center">
+          <div>
+            <h1 className="font-bold text-2xl text-[#37b47e]">University of Santo Tomas</h1>
+            <h2 className="font-bold text-sm text-[#37b47e]">Senior High School</h2>
+            <h2 className="font-bold text-sm">Accountacy, Business, and Management Strand</h2>
+          </div>
+          <div className="flex gap-x-4 justify-between w-[8rem]">
+            <Image src={ust_logo} height={56} alt="University of Santo Tomas Logo" />
+            <Image src={ustshs_logo} height={56} alt="University of Santo Tomas College of Information and Computing Sciences Logo" />
+          </div>
         </div>
-        <h1>University of Santo Tomas Senior High School</h1>
-        <h2>Accountacy, Business, and Management Strand</h2>
       </div>
     </div>
   );
@@ -56,8 +69,8 @@ const Education = () => {
 
 const Tools = () => {
   return (
-    <div>
-      <h1>Tools</h1>
+    <div className="mt-8">
+      <h1 className="font-bold text-3xl">Tools</h1>
       <div className="grid grid-cols-4 place-items-center">
         <p>Tool 1</p>
         <p>Tool 2</p>
@@ -76,8 +89,8 @@ const Tools = () => {
 
 const Experience = () => {
   return (
-    <div>
-      <h1>Experience</h1>
+    <div className="mt-8">
+      <h1 className="font-bold text-3xl">Experience</h1>
       <div>
         <p>Experience 1</p>
         <p>Experience 2</p>
@@ -89,8 +102,8 @@ const Experience = () => {
 
 const Certifications = () => {
   return (
-    <div>
-      <h1>Certifications</h1>
+    <div className="mt-8">
+      <h1 className="font-bold text-3xl">Certifications</h1>
       <div>
         <p>Certificate 1</p>
         <p>Certificate 2</p>
