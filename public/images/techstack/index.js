f1f262693060157b2ecd26239857b2b29449b4fe
cedricquitor <cedricquitor.dev@gmail.com>
2023-05-13 02:52:23 +0800

    Modified imports/exports

--- public/images/techstack/index.js
+++ public/images/techstack/index.js
@@ -2,5 +2,18 @@ import typescript_logo from "./typescript.png";
 import reactjs_logo from "./reactjs.png";
 import nodejs_logo from "./nodejs.png";
 import postgresql_logo from "./postgresql.png";
+import python_logo from "./python.png";
+import scikitlearn_logo from "./scikitlearn.png";
+import tensorflow_logo from "./tensorflow.png";
+import flask_logo from "./flask.png";
 
-export { typescript_logo, reactjs_logo, nodejs_logo, postgresql_logo };
+export {
+  typescript_logo,
+  reactjs_logo,
+  nodejs_logo,
+  postgresql_logo,
+  python_logo,
+  scikitlearn_logo,
+  tensorflow_logo,
+  flask_logo,
+};
