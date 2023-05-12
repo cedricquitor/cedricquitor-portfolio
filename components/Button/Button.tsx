6c06b62118a5084de4f500f3d555cccdb7efd62e
cedricquitor <cedricquitor.dev@gmail.com>
2023-05-13 06:18:05 +0800

    Button can now have secondary option

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,10 +1,21 @@
 type Props = {
   children: string;
   className?: string;
+  type?: "primary" | "secondary";
 };
 
-const Button = ({ children, className }: Props) => {
-  return <div className={`bg-[#37b47e] cursor-pointer px-10 py-6 w-fit text-center rounded-full text-white text-lg font-bold ${className}`}>{children}</div>;
+const Button = ({ children, className, type }: Props) => {
+  return (
+    <div
+      className={`${
+        type === "secondary"
+          ? "bg-transparent border-2 border-[#37b47e] text-[#37b47e] hover:text-white"
+          : "bg-[#37b47e] text-white"
+      } cursor-pointer px-10 py-6 w-fit text-center rounded-full text-lg font-bold ${className}`}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Button;
