18f06199e56ba7e17a94df0133145c7b70745e90
cedricquitor <cedricquitor.dev@gmail.com>
2023-05-15 00:44:11 +0800

    Now using props instead of harcoded text

--- components/ProjectItem.tsx
+++ components/ProjectItem.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import { dorav4 } from "../public/images/project";
 import Button from "./Button/Button";
 
@@ -8,8 +8,9 @@ interface TagItemProps {
 
 interface ProjectItemProps {
   title: string;
-  image: string;
+  image: StaticImageData;
   tags: string[];
+  description: string;
   projectLink?: string;
   githubLink?: string;
 }
@@ -23,13 +24,14 @@ const TagItem = (props: TagItemProps) => {
   );
 };
 
-const ProjectItem = () => {
+const ProjectItem = (props: ProjectItemProps) => {
+  const { title, image, tags, description, projectLink, githubLink } = props;
   return (
     <div className="grid grid-cols-12 space-x-4 rounded-md">
       {/* Left Side */}
       <div className="col-span-4 flex bg-red-100">
         <Image
-          src={dorav4}
+          src={image}
           alt="Project Item Image"
           height={320}
           className="border-2 border-gray-100 rounded-lg group-hover:border-gray-300"
@@ -38,29 +40,21 @@ const ProjectItem = () => {
       {/* Right Side */}
       <div className="col-span-8">
         <h1 className="text-2xl font-jakarta font-bold group-hover:text-green-400">
-          Disaster Online Reporting Application v4
+          {title}
         </h1>
-        <div className="flex space-x-2 mt-2">
-          <TagItem tag="JavaScript" />
-          <TagItem tag="ReactJS" />
-          <TagItem tag="Firebase" />
-          <TagItem tag="Google Maps API" />
+        <div className="flex flex-wrap mt-2 gap-2 w-3/4">
+          {tags.map((tag) => {
+            return <TagItem tag={tag} key={tag} />;
+          })}
         </div>
-        <p className="mt-4 text-sm text-gray-500">
-          Disaster Online Reporting Application (DORA) version 4 is an
-          Android-based mobile application that allows users to easily and
-          quickly report disasters in real-time. With this app, users can
-          provide critical information about the disaster, including its
-          location, type, and images allowing emergency responders to emergency
-          responders to quickly and effectively respond to the situation.
-        </p>
+        <p className="mt-4 text-sm text-gray-500">{description}</p>
         <div className="mt-4 flex space-x-4">
-          <Button className="py-2 w-64 text-center transition hover:bg-emerald-600">
-            Visit App
+          <Button className="py-2 w-54 text-center transition hover:bg-emerald-600">
+            Visit Project
           </Button>
           <Button
             type="secondary"
-            className="py-2 w-64 text-center transition hover:bg-emerald-600"
+            className="py-2 w-54 text-center transition hover:bg-emerald-600"
           >
             GitHub Repo
           </Button>
