dbc6edb4c223b5ec52eb6aa1736df59de92d8f78
cedricquitor <cedricquitor.dev@gmail.com>
2023-05-15 00:44:43 +0800

    Tools are grayscaled when not hovered

--- components/ToolItem.tsx
+++ components/ToolItem.tsx
@@ -7,8 +7,13 @@ interface Props {
 
 const ToolItem: React.FC<Props> = ({ name, image }) => {
   return (
-    <div className="flex flex-col items-center justify-center border border-gray-200 bg-gray-100 h-[12rem] rounded-2xl md:h-[9rem]">
-      <Image src={image} height={64} alt={`${name} Logo`} />
+    <div className="flex flex-col items-center justify-center border group border-gray-200 bg-gray-100 h-[12rem] rounded-2xl md:h-[9rem] relative">
+      <Image
+        src={image}
+        height={64}
+        alt={`${name} Logo`}
+        className="grayscale fill-emerald-500 transition duration-500 group-hover:grayscale-0"
+      />
       <h1 className="mt-2 text-sm text-gray-400">{name}</h1>
     </div>
   );
