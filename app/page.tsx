be31ddd6242807eb313efc1478bcf7f2493213dd
cedricquitor <cedricquitor.dev@gmail.com>
2023-05-21 04:07:52 +0800

    Added experience section

--- app/page.tsx
+++ app/page.tsx
@@ -39,15 +39,15 @@ const Hero = () => {
           <h2 className="font-bold text-2xl">Hi, I&apos;m Cedric👋</h2>
         </div>
         <h1 className="text-7xl font-bold">
-          Building web, data, and AI/ML applications.
+          Building web and AI/ML applications
         </h1>
         <div className="mt-2">
           <p className="text-2xl">
-            a <span className="font-bold">Machine Learning Engineer</span> based
-            in Manila, PH.
+            a <span className="font-bold">Software Engineer</span> based in
+            Manila, PH🇵🇭.
           </p>
           <p className="text-2xl">
-            I specialize in developing end-to-end ML pipelines.
+            I specialize in developing end-to-end products.
           </p>
         </div>
         <Link href="https://www.linkedin.com/in/cedricquitor/" target="_blank">
@@ -121,6 +121,19 @@ const About = () => {
         <h1 className="font-oswald text-3xl">What I do</h1>
         <div className="mt-4 space-y-8">
           <div>
+            <h2 className="text-2xl">Software Development</h2>
+            <p>
+              These are the languages and tools I mainly use in creating web
+              applications.
+            </p>
+            <div className="mt-4 grid grid-cols-2 gap-8 md:grid-cols-4">
+              {seTools.map((tool) => (
+                <ToolItem key={tool.name} name={tool.name} image={tool.image} />
+              ))}
+            </div>
+          </div>
+          {/* Here */}
+          <div>
             <h2 className="text-2xl">Machine Learning</h2>
             <p>
               These are the languages and tools I mainly use in experimenting
@@ -128,18 +141,6 @@ const About = () => {
             </p>
             <div className="mt-4 grid grid-cols-2 gap-8 md:grid-cols-4">
               {mlTools.map((tool) => (
-                <ToolItem key={tool.name} name={tool.name} image={tool.image} />
-              ))}
-            </div>
-          </div>
-          <div>
-            <h2 className="text-2xl">Software Engineering</h2>
-            <p>
-              These are the languages and tools I mainly use in creating web
-              applications.
-            </p>
-            <div className="mt-4 grid grid-cols-2 gap-8 md:grid-cols-4">
-              {seTools.map((tool) => (
                 <ToolItem key={tool.name} name={tool.name} image={tool.image} />
               ))}
             </div>
@@ -173,6 +174,7 @@ const Projects = () => {
   );
 };
 
+// Temporary
 const Education = () => {
   return (
     <div className="mt-8">
@@ -283,72 +285,145 @@ const Experience = () => {
   };
 
   return (
-    <div className="flex flex-row">
-      {/* Left Side */}
-      <div className="w-1/4 p-4">
-        <ul className="">
-          {companies.map((company) => (
-            <li key={company.id} className="">
-              <button
-                className={`${
-                  activeCompany.id === company.id
-                    ? "bg-emerald-50 text-emerald-600 border-l-green-600"
-                    : "border-l-gray-300"
-                } border-l-2 transition-all duration-500 w-full p-2 text-left`}
-                onClick={() => handleClick(company)}
+    <div>
+      <h1>Experience</h1>
+      <div className="flex flex-row">
+        {/* Left Side */}
+        <div className="w-1/4 p-4">
+          <ul className="">
+            {companies.map((company) => (
+              <li key={company.id} className="">
+                <button
+                  className={`${
+                    activeCompany.id === company.id
+                      ? "bg-emerald-50 text-emerald-600 border-l-green-600"
+                      : "border-l-gray-300"
+                  } border-l-2 transition-all duration-500 w-full p-2 text-left`}
+                  onClick={() => handleClick(company)}
+                >
+                  {company.name}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        {/* Right Side */}
+        <div className="w-3/4 p-4">
+          <h2 className="text-2xl font-bold">
+            {activeCompany?.role}{" "}
+            <span className="text-gray-600">
+              @{" "}
+              <Link
+                href={activeCompany?.link}
+                target="_blank"
+                className="transition hover:text-emerald-600"
               >
-                {company.name}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
-      {/* Right Side */}
-      <div className="w-3/4 p-4">
-        <h2 className="text-2xl font-bold">
-          {activeCompany?.role}{" "}
-          <span className="text-gray-600">
-            @{" "}
-            <Link
-              href={activeCompany?.link}
-              target="_blank"
-              className="transition hover:text-emerald-600"
-            >
-              {activeCompany?.name}
-            </Link>
-          </span>
-        </h2>
-        <p className="text-gray-400">{activeCompany?.duration}</p>
-        <ul className="mt-2">
-          {activeCompany.description.map((description, key) => {
-            return (
-              <div key={key} className="flex items-center space-x-2">
-                <MdKeyboardArrowRight className="text-emerald-600 text-xl" />
-                <li>{description}</li>
-              </div>
-            );
-          })}
-        </ul>
+                {activeCompany?.name}
+              </Link>
+            </span>
+          </h2>
+          <p className="text-gray-400">{activeCompany?.duration}</p>
+          <ul className="mt-2">
+            {activeCompany.description.map((description, key) => {
+              return (
+                <div key={key} className="flex items-center space-x-2">
+                  <MdKeyboardArrowRight className="text-emerald-600 text-xl" />
+                  <li>{description}</li>
+                </div>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   );
 };
 
 const AwardsAndCertificates = () => {
-  return <h1>AwardsAndCertificates</h1>;
+  // Hardcoded certification data for demonstration
+  const certifications = [
+    {
+      id: 1,
+      title: "Certification 1",
+      organization: "Organization A",
+      date: "May 2022",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      logo: "/organization-a-logo.png", // Replace with the actual path of the organization's logo image in your Next.js project
+    },
+    {
+      id: 2,
+      title: "Certification 2",
+      organization: "Organization B",
+      date: "July 2022",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      logo: "/organization-b-logo.png", // Replace with the actual path of the organization's logo image in your Next.js project
+    },
+    // Add more certification objects as needed
+  ];
+
+  return (
+    <div className="container mx-auto py-8">
+      <h2 className="text-3xl font-bold mb-6">Certifications</h2>
+      <div className="grid gap-6">
+        {certifications.map((certification) => (
+          <div
+            key={certification.id}
+            className="bg-white shadow-lg rounded-lg overflow-hidden p-6 flex"
+          >
+            <div className="flex-1">
+              <h3 className="text-xl font-semibold mb-2">
+                {certification.title}
+              </h3>
+              <p className="text-emerald-500 mb-4">
+                {certification.organization}
+              </p>
+              <p className="text-sm text-gray-500 mb-4">
+                Achieved: {certification.date}
+              </p>
+              <p className="text-gray-800">{certification.description}</p>
+            </div>
+            <div className="ml-4">
+              <Image
+                src={certification.logo}
+                alt={`${certification.organization} Logo`}
+                width={48}
+                height={48}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 const Contact = () => {
   return (
-    <div className="">
-      {/* Get In Touch */}
-      {/* Contact Form */}
-      <h1 className="font-oswald text-3xl">Get in touch</h1>
-      <h1 className="mt-4">
-        Get in touch to discuss your next software project. Let&apos;s make it
-        happen.
-      </h1>
-      <h1>Add contact me form here</h1>
+    <div className="container mx-auto py-8">
+      <h2 className="text-3xl font-bold mb-6">Get in Touch</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div>
+          <h3 className="text-xl font-semibold mb-2">Contact Information</h3>
+          <p>Email: cedricjohn.quitor.cics@ust.edu.ph</p>
+          <p>Phone: +63 (916) 597 5656</p>
+          <p>Location: Manila, Philippines</p>
+        </div>
+        <div>
+          <h3 className="text-xl font-semibold mb-2">Social Media</h3>
+          <p>
+            LinkedIn:{" "}
+            <a href="https://linkedin.com/in/cedricquitor">
+              linkedin.com/in/cedricquitor
+            </a>
+          </p>
+          <p>
+            GitHub:{" "}
+            <a href="https://github.com/cedricquitor">
+              github.com/cedricquitor
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
