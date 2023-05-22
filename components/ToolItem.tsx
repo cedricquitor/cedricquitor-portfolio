77d97949886833a0169227953a77aba7b40287a5
cedricquitor <cedricquitor.dev@gmail.com>
2023-05-22 13:12:03 +0800

    Added hover effects

--- components/ToolItem.tsx
+++ components/ToolItem.tsx
@@ -7,14 +7,16 @@ interface Props {
 
 const ToolItem: React.FC<Props> = ({ name, image }) => {
   return (
-    <div className="flex flex-col items-center justify-center border group border-gray-200 bg-gray-100 h-[12rem] rounded-2xl md:h-[9rem] relative">
+    <div className="flex flex-col items-center justify-center border group border-gray-200 bg-gray-100 h-[12rem] rounded-2xl md:h-[9rem] relative transition duration-500 hover:bg-gray-200 hover:border-emerald-600">
       <Image
         src={image}
         height={64}
         alt={`${name} Logo`}
         className="grayscale fill-emerald-500 transition duration-500 group-hover:grayscale-0"
       />
-      <h1 className="mt-2 text-sm text-gray-400">{name}</h1>
+      <h1 className="mt-2 text-sm text-gray-400 transition duration-500 group-hover:text-emerald-600">
+        {name}
+      </h1>
     </div>
   );
 };
