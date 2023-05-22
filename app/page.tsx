4a25efecbe177b7837ea808b016ff749120f9f42
cedricquitor <cedricquitor.dev@gmail.com>
2023-05-22 14:51:06 +0800

    Refactored projects into a component

--- app/page.tsx
+++ app/page.tsx
@@ -13,29 +13,7 @@ import Hero from "../components/Hero";
 import About from "../components/About";
 import Skills from "../components/Skills";
 import Experience from "../components/Experience";
-
-const Projects = () => {
-  const dorav4Desc =
-    "Disaster Online Reporting Application (DORA) version 4 is an Android-based mobile application that allows users to easily and quickly report disasters in real-time. With this app, users can provide critical information about the disaster, including its location, type, and images allowing emergency responders to emergency responders to quickly and effectively respond to the situation.";
-  return (
-    <div className="">
-      {/* My Works */}
-      <h1 className="font-bold text-3xl">Projects</h1>
-      <div className="mt-4 grid grid-cols-2">
-        <h1>DORA</h1>
-        <h1>imaginkraf</h1>
-      </div>
-      <div className="mt-4">
-        <ProjectItem
-          title="Disaster Online Reporting Application v4"
-          image={dorav4}
-          tags={["JavaScript", "ReactJS", "Firebase", "Google Maps API"]}
-          description={dorav4Desc}
-        />
-      </div>
-    </div>
-  );
-};
+import Projects from "../components/Projects";
 
 const AwardsAndCertificates = () => {
   // Hardcoded certification data for demonstration
