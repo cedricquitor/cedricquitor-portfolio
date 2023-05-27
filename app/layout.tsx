a7e09e67d4a6b277fa75503ab0abfa7a22a725b0
cedricquitor <cedricquitor.dev@gmail.com>
2023-05-27 17:48:43 +0800

    Changed font to Inter

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import Navbar from "../components/Navbar";
 import "./globals.css";
-import { Plus_Jakarta_Sans } from "@next/font/google";
+import { Inter } from "@next/font/google";
 
-const jakarta = Plus_Jakarta_Sans({
-  variable: "--jakarta-font",
+const inter = Inter({
+  variable: "--inter-font",
   weight: ["200", "300", "400", "500", "600", "700", "800"],
 });
 
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${jakarta.variable}`}>
+    <html lang="en" className={`${inter.variable}`}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body suppressHydrationWarning={true}>
         {/* Navbar */}
         <Navbar />
-        <div className={`mx-auto max-w-4xl px-6 md:px-0 font-jakarta`}>
+        <div className={`mx-auto max-w-4xl px-6 md:px-0 font-inter`}>
           {children}
         </div>
       </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
-        jakarta: "var(--jakarta-font)",
+        inter: "var(--inter-font)",
       },
     },
   },
