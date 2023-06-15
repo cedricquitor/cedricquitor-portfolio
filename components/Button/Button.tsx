7b5e41252077ab8339a14646d58dc1f1485d8532
cedricquitor <cedricquitor.dev@gmail.com>
2023-06-16 05:05:23 +0800

    Added optional onClick

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -4,9 +4,16 @@ type Props = {
   children: ReactNode | string;
   className?: string;
   type?: "primary" | "secondary";
+  onClick?: () => void; // Add the onClick prop
 };
 
-const Button = ({ children, className, type }: Props) => {
+const Button = ({ children, className, type, onClick }: Props) => {
+  const handleClick = () => {
+    if (onClick) {
+      onClick();
+    }
+  };
+
   return (
     <div
       className={`${
@@ -14,6 +21,7 @@ const Button = ({ children, className, type }: Props) => {
           ? "bg-transparent border-2 border-[#37b47e] text-[#37b47e] hover:text-white"
           : "bg-[#37b47e] text-white"
       } cursor-pointer px-4 py-2 w-fit text-center rounded-2xl text-lg font-bold ${className}`}
+      onClick={handleClick} // Attach the onClick event handler to the div
     >
       {children}
     </div>
