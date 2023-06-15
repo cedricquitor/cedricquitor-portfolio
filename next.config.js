44d4fdc10bee1d051368a8512a0f619969d3a0cd
cedricquitor <cedricquitor.dev@gmail.com>
2023-06-16 05:59:07 +0800

    Supressed warnings

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.infrastructureLogging = {
+      level: "error",
+    };
+
+    return config;
+  },
+  reactStrictMode: true,
   experimental: {
     appDir: true,
     fontLoaders: [
