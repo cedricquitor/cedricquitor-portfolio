5840bed46d14faab3a054b618ccd16db0db7f08f
cedricquitor <cedricquitor.dev@gmail.com>
2023-06-17 06:03:25 +0800

    Added boston housing image and import

--- public/images/project/index.js
+++ public/images/project/index.js
@@ -2,5 +2,6 @@ import dorav4 from "./dorav4.png";
 import imaginkraf from "./imaginkraf.png";
 import botomasino from "./botomasino.png";
 import trinkets from "./trinkets.png";
+import boston from "./boston_housing.png";
 
-export { dorav4, imaginkraf, botomasino, trinkets };
+export { dorav4, imaginkraf, botomasino, trinkets, boston };
