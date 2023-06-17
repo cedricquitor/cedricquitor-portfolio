384c4dcee5ad85fd4ff92f8d699a96a86c0b0af7
cedricquitor <cedricquitor.dev@gmail.com>
2023-06-17 08:37:12 +0800

    Fixed uneven primary and seconday

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -18,9 +18,9 @@ const Button = ({ children, className, type, onClick }: Props) => {
     <div
       className={`${
         type === "secondary"
-          ? "bg-transparent border-2 border-emerald-500 text-emerald-500 hover:text-white"
+          ? "bg-transparent text-emerald-500 hover:text-white"
           : "bg-emerald-500 text-white"
-      } cursor-pointer px-4 py-2 w-fit text-center rounded-2xl text-lg font-bold ${className}`}
+      } cursor-pointer px-4 py-2 w-fit text-center rounded-2xl text-lg font-bold border-2 border-emerald-500 ${className}`}
       onClick={handleClick} // Attach the onClick event handler to the div
     >
       {children}
